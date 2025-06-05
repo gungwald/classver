6f004b3be9779f6a3ae92f8a067c8e0d0c1f7463
Bill Chatfield <bill_chatfield@yahoo.com>
2025-06-05 08:47:41 -0400

    Wrapper scripts search for jar

--- src/main/dist/bin/classver.bat
+++ src/main/dist/bin/classver.bat
@@ -1,19 +1,29 @@
 @echo off
 
-setlocal
+setlocal EnableDelayedExpansion
 
-set THIS=%~n0
-set THIS_DIR=%~dp0
-set LIB_DIR=%THIS_DIR%..\lib
+set THIS_SCRIPT=%~n0
+set THIS_SCRIPT_DIR=%~dp0
+set LIB_DIR=%THIS_SCRIPT_DIR%..\lib\
+set JAR_NAME=%THIS_SCRIPT%.jar
+set BUILD_LIB_DIR=%THIS_SCRIPT_DIR%..\..\..\..\build\libs\
 
-rem Find the highest version of the jar.
-for %%j in ("%LIB_DIR%\%THIS%-*.jar") do (
-	set JAR=%%j
+rem Find the the jar.
+for %%d in ("%THIS_SCRIPT_DIR%" "%LIB_DIR%" "%BUILD_LIB_DIR%") do (
+    set MAYBE_EXISTS_JAR=%%~d%JAR_NAME%
+    if exist "!MAYBE_EXISTS_JAR!" (
+        if defined DEBUG echo JAR exists: !MAYBE_EXISTS_JAR!
+	    set JAR=!MAYBE_EXISTS_JAR!
+	    goto :runJar
+	) else (
+        if defined DEBUG echo JAR does not exist: !MAYBE_EXISTS_JAR!
+    )
 )
+echo No %JAR_NAME% file found
+goto :EOF
 
-if exist "%JAR%" (
-	java -jar "%JAR%" 
-) else (
-	echo No %THIS% jar file found in %LIB_DIR%.
-)
+:runJar
+if defined DEBUG echo Running %JAR%
+java -jar "%JAR%" %*
+
 endlocal
